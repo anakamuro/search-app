f20135e7a558898ffbe3a17b546acf537b435640
Akihiko Nakamura <anakamuro@earthlink.net>
2024-04-03 08:50:59 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react"
+import {data} from "./data.js"
+
 
 function App() {
+  // const [data, setData] = useState([])
+  const [search, setSearch] = useState('')
+  console.log(search)
+
+  // useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/users')
+  //      .then((res) => {
+  //       return res.json()
+  //      })
+  //      .then((json) => {
+  //       const results = json.filter((user)=> {
+  //         return user && user.name.toLowerCase().includes(value)
+  //       })
+  //       console.log(results)
+  //      })
+  // }, []);
+
+  console.log(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>The personal information of the employee in the company</h2>
+        <input onChange={(e) => setSearch(e.target.value)}/>
+        <div className="multi">
+        {data
+           .filter((item) => {
+            if (search === ""){
+              return item
+            } else if (item.name.toLowerCase().includes(search.toLowerCase())) {
+              return item
+            }
+           })
+        .map(item => (
+        <div className="box" key={item.id}>
+        <label>Name</label>
+        <span>{item.name}</span>
+        <hr/>
+        <label>Email</label>
+        <span>{item.email}</span>
+        <hr/>
+        <label>Address</label>
+        <span>{item.address.street} {item.address.suite} {item.address.city} {item.address.zipcode}</span>
+        </div>
+        ))}
+        </div>
+      </div>
     </div>
   );
 }
